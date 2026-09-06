e0f434478f585a37c1f48eb8958f029840876e45
Ale Szoska <alexszoska@MacBook-Pro-Ale.local>
2026-09-06 12:56:07 +0200

    Inicjalizacja projektu PWA

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// Service Worker dla Parafii św. Jana Chrzciciela
+const CACHE_NAME = 'parafia-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalacja – cache statycznych zasobów
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Aktywacja – usunięcie starych cache'ów
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch – strategia Network First, fallback do cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
